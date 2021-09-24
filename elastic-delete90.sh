db9041ab226bf39e66b2b86ac4ed3b407ff1b267
Paulo Daniel Ambrosio dos Santos <33867540+pdaambrosio@users.noreply.github.com>
2021-09-24 18:32:23 -0300

    fix space issue CodeFactor

--- elastic-delete90.sh
+++ elastic-delete90.sh
@@ -6,10 +6,9 @@ validate=$(curl -s -XGET http://$HOSTNAME:9200/_cat/indices?pretty |grep logstas
 url=$(curl -s -XDELETE http://$HOSTNAME:9200/logstash-$date?pretty -H 'Content-Type:application/json')
 
 if [ "$validate" != "logstash-$date" ]; then
-                echo "index does not exist"
+echo "index does not exist"
 elif [ "$master" != "$HOSTNAME" ]; then
-                echo "it's not the master"
+echo "it's not the master"
 else
-                echo $url
-
+echo $url
 fi
