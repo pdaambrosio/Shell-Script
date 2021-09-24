306e8492eed4dddec4ca2f02515b76e8d94a75b6
Paulo Daniel Ambrosio dos Santos <33867540+pdaambrosio@users.noreply.github.com>
2021-09-24 18:36:00 -0300

    fix egrep is a deprecated command CodeFactor

--- elastic-restore.sh
+++ elastic-restore.sh
@@ -3,7 +3,7 @@
 DATE=$(date +%Y.%m.%d -d "-15 days")
 MASTER=$(curl -s -XGET $HOSTNAME:9200/_cat/master?pretty |cut -d " " -f4)
 VALIDATE=$(curl -s -XGET $HOSTNAME:9200/_cat/indices?pretty |grep logstash-$DATE |awk '{print$3}')
-SNAPSHOT=$(curl -s -XGET $HOSTNAME:9200/_cat/snapshots/backup?pretty |egrep "SUCCESS" |cut -d " " -f 1 |sort |tail -n2 |head -n1)
+SNAPSHOT=$(curl -s -XGET $HOSTNAME:9200/_cat/snapshots/backup?pretty |grep -E "SUCCESS" |cut -d " " -f 1 |sort |tail -n2 |head -n1)
 
 printf "DATA --- $DATE\n MASTER ---- $MASTER\n VALIDAR ---- $VALIDATE\n CURL ----- $CURL\n SNAPSHOT ---- $SNAPSHOT\n\n"
 
